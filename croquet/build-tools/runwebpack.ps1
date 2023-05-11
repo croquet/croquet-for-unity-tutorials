5f6b36aec37652486f3385d1ce30758297877db4
Aran Lunzer <aran@acm.org>
2023-05-11 00:31:44 -0700

    (Windows) warning if webpack not found

--- croquet/build-tools/runwebpack.ps1
+++ croquet/build-tools/runwebpack.ps1
@@ -5,6 +5,14 @@
 $NODE=$args[0]
 $APPNAME=$args[1]
 
+if (!(Test-Path ..\node_modules\webpack)) {
+  Write-Warning "Cannot find webpack.  Did you do 'npm install'?"
+  Exit
+}
+
 get-content sources\index-node.generic.js | % { $_ -replace '__APP_SOURCE__' , "../../$APPNAME" } | set-content sources\index-node.tmp.js
 
 & "$NODE" ..\node_modules\webpack\bin\webpack.js --config webpack.config.js --mode development --env appName="$APPNAME" --env buildTarget=node --no-color
+
+# this output will be read by CroquetBuilder, to keep a record of the webpack process id
+echo "webpack-exit=$LASTEXITCODE"
